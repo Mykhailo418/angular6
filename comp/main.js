51d46a6f8d0962e79982e0f686f99233e6acb7b8
Mykhailo418 <smmichael418@gmail.com>
2018-11-25 19:21:36 +0200

    Pipes: impure pipe

--- comp/main.js
+++ comp/main.js
@@ -15549,7 +15549,9 @@ var FilterPipe = (function () {
 }());
 FilterPipe = __decorate([
     core_1.Pipe({
-        name: 'filter'
+        name: 'filter',
+        pure: false // !IMPORTANT: it can make perfomance issues. This param allows to rerun pipe when anything was changed on the page.
+        // The most populat example: when value(in this case it is array) was changed - was added new string or deleted, it will react on this chnage otherwise it will not rerun pipe
     })
 ], FilterPipe);
 exports.FilterPipe = FilterPipe;
--- dist/js/pipes/filter.pipe.ts
+++ dist/js/pipes/filter.pipe.ts
@@ -1,7 +1,9 @@
 import { Pipe, PipeTransform } from '@angular/core';
 
 @Pipe({
-  name: 'filter'
+  name: 'filter',
+  pure: false // !IMPORTANT: it can make perfomance issues. This param allows to rerun pipe when anything was changed on the page.
+              // The most populat example: when value(in this case it is array) was changed - was added new string or deleted, it will react on this chnage otherwise it will not rerun pipe
 })
 export class FilterPipe implements PipeTransform{
   transform(value: any, searchText: string){
