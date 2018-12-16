4edcebd0ae68e6bfe8811f6bf7a76b9c99b956cd
Mykhailo418 <smmichael418@gmail.com>
2018-12-16 15:28:19 +0200

    Dependencies updating, Angular 6 -> Angular 7

--- webpack.config.js
+++ webpack.config.js
@@ -5,14 +5,15 @@ const html_path = __dirname + '/dist/html/';
 const webpack = require('webpack');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const ExtractTextPlugin = require('extract-text-webpack-plugin');
+const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
 const helpers = require('./helpers');
 
 module.exports = {
 	context: __dirname,
 	entry: {
-		main: js_path + 'main.ts',
-		vendor: js_path + 'vendor.ts',
-		polyfills: js_path + 'polyfills.ts'
+			polyfills: js_path + 'polyfills.ts',
+			vendor: js_path + 'vendor.ts',
+			main: js_path + 'main.ts'
 	},
 	resolve: {
 	    extensions: ['.ts', '.js']
@@ -27,8 +28,8 @@ module.exports = {
 		    test: /\.ts$/,
 		    loaders: [
 	          {
-	            loader: 'awesome-typescript-loader', 
-	          } , 
+	            loader: 'awesome-typescript-loader',
+	          } ,
 	          'angular2-template-loader'
 	        ]
 		  },
@@ -46,19 +47,29 @@ module.exports = {
 	plugins: [
 	    // Workaround for angular/angular#11580
 	    new webpack.ContextReplacementPlugin(
-	      // The (\\|\/) piece accounts for path separators in *nix and Windows
+	      // The (\\|\/) piece accounts for path separators in *nix and Windows 
 	      /angular(\\|\/)core(\\|\/)@angular/,
 	      helpers.root('./dist'), // location of your src
 	      {} // a map of your routes
 	    ),
 
-	    new webpack.optimize.CommonsChunkPlugin({
+	    /*new webpack.optimize.CommonsChunkPlugin({
 	      name: ['main', 'vendor', 'polyfills']
-	    }),
+	    }),*/
 
 	    new HtmlWebpackPlugin({
 	      template: html_path + 'index.html',
 	      filename: '../index.html'
 	    })
-	]
+	],
+	optimization: {
+    minimizer: [new UglifyJsPlugin({
+			uglifyOptions: {
+				output: {
+					ascii_only: true,
+					beautify: false
+				}
+			}
+		})]
+  }
 };
