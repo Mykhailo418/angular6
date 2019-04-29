bb5cffdb9cd3bd84b4dffd4b9b9e1edda4b443a1
Mykhailo418 <smmichael418@gmail.com>
2019-04-29 17:59:14 +0300

    Animations: three state animation, shrink box

--- dist/js/animation/animationPage.component.ts
+++ dist/js/animation/animationPage.component.ts
@@ -3,6 +3,7 @@ import { trigger, state, style, transition, animate } from '@angular/animations'
 
 const NORMAL_STATE = 'normal';
 const HIGHLIGHTED_STATE = 'highkighted';
+const SHRINK_STATE = 'shrink';
 
 @Component({
 	selector: 'animation-pagr',
@@ -19,15 +20,36 @@ const HIGHLIGHTED_STATE = 'highkighted';
       }) ),
       transition(`${NORMAL_STATE} <=> ${HIGHLIGHTED_STATE}`, animate(300)),
       //transition(`${HIGHLIGHTED_STATE} => ${NORMAL_STATE}`, animate(800)),
+    ]),
+    trigger('wildState', [
+      state(NORMAL_STATE, style({
+        'background-color': 'black',
+        transform: 'translateX(0) scale(1)'
+      }) ),
+      state(HIGHLIGHTED_STATE, style({
+        'background-color': 'red',
+        transform: 'translateX(100px) scale(1)'
+      }) ),
+      state(SHRINK_STATE, style({
+        'background-color': 'green',
+        transform: 'translateX(0) scale(0.5)'
+      }) ),
+      transition(`${NORMAL_STATE} <=> ${HIGHLIGHTED_STATE}`, animate(300)),
+      transition(`${SHRINK_STATE} <=> *`, animate(500)),
     ])
   ]
 })
 export class AnimationPageComponent {
   state: string = NORMAL_STATE;
-
+  wildState: string = NORMAL_STATE;
 	constructor(){}
 
   onAnimate(){
     this.state = (this.state === NORMAL_STATE) ? HIGHLIGHTED_STATE : NORMAL_STATE;
+    this.wildState = (this.wildState === NORMAL_STATE) ? HIGHLIGHTED_STATE : NORMAL_STATE;
+  }
+
+  onShrink(){
+    this.wildState = (this.wildState !== SHRINK_STATE) ? SHRINK_STATE : NORMAL_STATE;
   }
 }
